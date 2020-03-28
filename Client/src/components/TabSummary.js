42752a7d93be8304507c30e4a19216e88e1b209a
Daniel Lewandowski <daniel@nextpagesoft.net>
2020-03-28 19:34:50 +0100

    Client: prototype chats with apexcharts

--- Client/src/components/TabSummary.js
+++ Client/src/components/TabSummary.js
@@ -1,20 +1,99 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-//import { VictoryChart, VictoryBar, VictoryLine, VictoryAxis, VictoryTheme } from 'victory';
-import { XYPlot, LineSeries, VerticalGridLines, XAxis, YAxis, HorizontalGridLines } from 'react-vis';
+import Chart from "react-apexcharts";
 
-const data = [
-  { x: 0, y: 8 },
-  { x: 1, y: 5 },
-  { x: 2, y: 4 },
-  { x: 3, y: 9 },
-  { x: 4, y: 1 },
-  { x: 5, y: 7 },
-  { x: 6, y: 6 },
-  { x: 7, y: 3 },
-  { x: 8, y: 2 },
-  { x: 9, y: 0 }
-];
+const data1 = {
+  options: {
+    chart: {
+      id: "basic-bar"
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+    }
+  },
+  series: [
+    {
+      name: "series-1",
+      data: [30, 40, 45, 50, 49, 60, 70, 91]
+    }
+  ]
+};
+
+const data2 = {
+  series: [
+    {
+      name: "Series 1",
+      data: [{
+        x: 'W1',
+        y: 22
+      }, {
+        x: 'W2',
+        y: 29
+      }, {
+        x: 'W3',
+        y: 13
+      }, {
+        x: 'W4',
+        y: 32
+      }]
+    },
+    {
+      name: "Series 2",
+      data: [{
+        x: 'W1',
+        y: 43
+      }, {
+        x: 'W2',
+        y: 43
+      }, {
+        x: 'W3',
+        y: 43
+      }, {
+        x: 'W4',
+        y: 43
+      }]
+    }
+  ],
+  plotOptions: {
+    dataLabels: {
+      enabled: false
+    },
+    title: {
+      text: 'HeatMap Chart with Color Range'
+    },
+    heatmap: {
+      shadeIntensity: 0.5,
+
+      colorScale: {
+        ranges: [{
+            from: -30,
+            to: 5,
+            name: 'low',
+            color: '#00A100'
+          },
+          {
+            from: 6,
+            to: 20,
+            name: 'medium',
+            color: '#128FD9'
+          },
+          {
+            from: 21,
+            to: 45,
+            name: 'high',
+            color: '#FFB200'
+          },
+          {
+            from: 46,
+            to: 55,
+            name: 'extreme',
+            color: '#FF0000'
+          }
+        ]
+      }
+    }
+  }
+}
 
 const TabSummary = () => {
 
@@ -35,13 +114,8 @@ const TabSummary = () => {
   return (
     <Grid container>
       <Grid item xs={12}>
-        <XYPlot height={300} width={300}>
-          <LineSeries data={data} />
-          <VerticalGridLines />
-          <HorizontalGridLines />
-          <XAxis />
-          <YAxis />
-        </XYPlot>
+        <Chart options={data1.options} series={data1.series} type='line' width='100%' height={300} />
+        <Chart options={data2.plotOptions} series={data2.series} title={data2.title} type='heatmap' width='100%' height={300} />
           {/*
           <VictoryChart
             // domainPadding will add space to each side of VictoryBar to
