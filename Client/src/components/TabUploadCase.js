04e6904442ec3950963002fa640aef9b725394e8
Daniel Lewandowski <daniel@nextpagesoft.net>
2020-04-04 19:33:32 +0200

    Client: add prototype for case-based uploaded data details

--- Client/src/components/TabUploadCase.js
+++ Client/src/components/TabUploadCase.js
@@ -10,8 +10,10 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import Btn from './Btn';
+import Paper from '@material-ui/core/Paper';
 
 const userStyles = makeStyles({
   header: {
@@ -35,7 +37,7 @@ const TabUploadCase = (props) => {
   return (
     <TabPanel>
       <Grid container spacing={2}>
-        <Grid item xs={2}>
+        <Grid item xs={3}>
           <input style={{ display: 'none' }} id='caseUploadBtn' className='uploadBtn' type='file' />
           <Tooltip title='Select case-based data file'>
             <label htmlFor='caseUploadBtn'>
@@ -51,32 +53,42 @@ const TabUploadCase = (props) => {
               color='secondary'
             />
           }
+          <Typography variant='body2' color='textSecondary'>
+            Maximum file size: 70MB<br />
+            Supported files types: rds, txt, csv, xls, xlsx (uncompressed and zip archives)
+          </Typography>
         </Grid>
-        <Grid item xs={5}>
-          <Table>
-            <TableBody>
-              <TableRow>
-                <TableCell className={classes.header}>File name</TableCell><TableCell>{appManager.caseBasedDataFileName}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className={classes.header}>File size</TableCell><TableCell>{appManager.caseBasedDataFileSize}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className={classes.header}>File type</TableCell><TableCell>{appManager.caseBasedDataFileType}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className={classes.header}>File path</TableCell><TableCell>{appManager.caseBasedDataPath}</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </Grid>
-        <Grid item xs={5}>
-          <Table>
-            <TableBody>
-              <TableRow><TableCell className={classes.header}>Column names</TableCell></TableRow>
-              <TableRow><TableCell>...</TableCell></TableRow>
-            </TableBody>
-          </Table>
+        <Grid item xs={9}>
+          <Paper style={{padding: 10}}>
+            <Typography variant='overline' display="block" component='div'>Uploaded file details</Typography>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <Table>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell className={classes.header}>File name</TableCell><TableCell>{appManager.caseBasedDataFileName}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell className={classes.header}>File size</TableCell><TableCell>{appManager.caseBasedDataFileSize}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell className={classes.header}>File type</TableCell><TableCell>{appManager.caseBasedDataFileType}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell className={classes.header} style={{ borderBottom: 'none' }}>File path</TableCell><TableCell style={{ borderBottom: 'none' }}>{appManager.caseBasedDataPath}</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </Grid>
+              <Grid item xs={6}>
+                <Table>
+                  <TableBody>
+                    <TableRow><TableCell className={classes.header}>Column names</TableCell></TableRow>
+                  </TableBody>
+                </Table>
+              </Grid>
+            </Grid>
+          </Paper>
         </Grid>
       </Grid>
     </TabPanel>
