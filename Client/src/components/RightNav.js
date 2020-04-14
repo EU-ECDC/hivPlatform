b1d1cb67d3d9a17d6bfb3a5619653caf36c25d8b
Daniel Lewandowski <daniel@nextpagesoft.net>
2020-04-14 09:53:24 +0200

    Client: prototype outputs tab

--- Client/src/components/RightNav.js
+++ Client/src/components/RightNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
 const RightNav = (props) => {
@@ -9,9 +10,13 @@ const RightNav = (props) => {
   return (
     <Drawer anchor='right' open={open} onClose={onClose}>
       <Box width={300} p={2}>
-        <Typography variant='h6'>
-          Settings
-        </Typography>
+        <Typography variant='h6'>Options:</Typography>
+        <ul>
+          <li><Link href="#">Save state</Link></li>
+          <li><Link href="#">Load state</Link></li>
+          <li><Link href="#">Set seed</Link></li>
+          <li><Link href="#">Open new instance in separate tab</Link></li>
+        </ul>
       </Box>
     </Drawer>
   )
--- Client/src/components/TabOutputs.js
+++ Client/src/components/TabOutputs.js
@@ -1,15 +1,72 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Skel from './Skeleton';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Link from '@material-ui/core/Link';
+import TabPanel from './TabPanel';
 
 const TabOutputs = () => {
 
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Skel />
+    <TabPanel>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Box display="flex" justifyContent="flex-end">
+            <Button size='small' color='primary'>Next step</Button>
+          </Box>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant='body1'>
+            HIV Modelling data downloads
+          </Typography>
+        </Grid>
+        <Grid item xs={9}>
+          <Paper style={{ padding: 10 }}>
+            <Table>
+              <TableBody>
+                <TableRow hover>
+                  <TableCell width={150}>R</TableCell>
+                  <TableCell><Link href="#">rds file</Link></TableCell>
+                </TableRow>
+                <TableRow hover>
+                  <TableCell width={100}>HIV Modelling tool</TableCell>
+                  <TableCell><Link href="#">zipped csv files</Link></TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Divider light style={{ margin: '30px 0' }} />
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant='body1'>
+            Reporting delays downloads
+          </Typography>
+        </Grid>
+        <Grid item xs={9}>
+          <Paper style={{ padding: 10 }}></Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Divider light style={{ margin: '30px 0' }} />
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant='body1'>
+            Adjusted data downloads
+          </Typography>
+        </Grid>
+        <Grid item xs={9}>
+          <Paper style={{ padding: 10 }}></Paper>
+        </Grid>
       </Grid>
-    </Grid>
+    </TabPanel>
   );
 };
 
