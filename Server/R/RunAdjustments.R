b532dbcbc40e21284fd675e98bf49605efc3032a
Daniel Lewandowski <daniel@nextpagesoft.net>
2020-05-14 20:20:05 +0200

    Use new message printing functions in RunAdjustments

--- Server/R/RunAdjustments.R
+++ Server/R/RunAdjustments.R
@@ -3,11 +3,12 @@
 #' Execute adjustments specified in \code{adjustmentFileNames}.
 #'
 #' @param data data table object on which adjustments should be applied. Required.
-#' @param adjustmentSpecs List of adjustment specifications to execute. Optional. Default = \code{list()}.
+#' @param adjustmentSpecs List of adjustment specifications to execute. Optional.
+#'   Default = \code{list()}.
 #' @param diagYearRange Numeric vector of length two with lower and upper bound for diagnosis year.
 #'   Optional. Default = \code{NULL}.
-#' @param notifQuarterRange Numeric vector of length two with lower and upper bound for notification quarter.
-#'   Optional. Default = \code{NULL}.
+#' @param notifQuarterRange Numeric vector of length two with lower and upper bound for
+#'   notification quarter. Optional. Default = \code{NULL}.
 #' @param seed Random seed. Optional. Default = NULL
 #'
 #' @return data table object after adjustments applied.
@@ -18,8 +19,13 @@
 #' }
 #'
 #' @export
-RunAdjustments <- function(data, adjustmentSpecs = list(), diagYearRange = NULL, notifQuarterRange = NULL, seed = NULL)
-{
+RunAdjustments <- function(
+  data,
+  adjustmentSpecs = list(),
+  diagYearRange = NULL,
+  notifQuarterRange = NULL,
+  seed = NULL
+) {
   stopifnot(!missing(data))
 
   # Data table performs many operations by reference.
@@ -27,7 +33,9 @@ RunAdjustments <- function(data, adjustmentSpecs = list(), diagYearRange = NULL,
   # procedures.
   data <- list(Table = copy(data))
   if (!is.null(diagYearRange)) {
-    data$Table <- data$Table[DateOfDiagnosisYear %between% diagYearRange | is.na(DateOfDiagnosisYear)]
+    data$Table <- data$Table[
+      DateOfDiagnosisYear %between% diagYearRange | is.na(DateOfDiagnosisYear)
+    ]
   }
   if (!is.null(notifQuarterRange)) {
     data$Table <- data$Table[NotificationTime %between% notifQuarterRange | is.na(NotificationTime)]
@@ -46,31 +54,30 @@ RunAdjustments <- function(data, adjustmentSpecs = list(), diagYearRange = NULL,
       adjustmentSpec$Key <- caption
     }
 
-    cli::cli_h1('Adjustment {caption}')
+    PrintH1('Adjustment {caption}')
 
     # Extract parameters for better visibility.
-    parameters <- GetParamInfoFromAdjustSpec(adjustmentSpec$Parameters,
-                                             infoType = "value")
+    parameters <- GetParamInfoFromAdjustSpec(adjustmentSpec$Parameters, infoType = "value")
 
-    cli::cli_h2('Parameters')
-    print(setNames(as.character(parameters),
-                   names(parameters)))
+    PrintH2('Parameters')
+    print(setNames(as.character(parameters), names(parameters)))
     cat("\n")
 
-    cli::cli_h2('Adjustment text output')
+    PrintH2('Adjustment text output')
 
     # Run adjustment function
-    output <- adjustmentSpec$AdjustmentFunction(inputData = data$Table,
-                                                parameters = parameters)
+    output <- adjustmentSpec$AdjustmentFunction(inputData = data$Table, parameters = parameters)
 
-    data <- list(Table = output$Table,
-                 Artifacts = output$Artifacts,
-                 Parameters = parameters,
-                 RunIdx = i,
-                 Name = adjustmentSpec$Name,
-                 Type = adjustmentSpec$Type,
-                 SubType = adjustmentSpec$SubType,
-                 TimeStamp = GetTimeStamp())
+    data <- list(
+      Table = output$Table,
+      Artifacts = output$Artifacts,
+      Parameters = parameters,
+      RunIdx = i,
+      Name = adjustmentSpec$Name,
+      Type = adjustmentSpec$Type,
+      SubType = adjustmentSpec$SubType,
+      TimeStamp = GetTimeStamp()
+    )
 
     if ("Imputation" %in% colnames(data$Table)) {
       setorderv(data$Table, "Imputation")
@@ -79,7 +86,7 @@ RunAdjustments <- function(data, adjustmentSpecs = list(), diagYearRange = NULL,
     # Store intermediate results for later reference
     results[[adjustmentSpec$Key]] <- data
 
-    cli::cli_alert_success('Done with adjustment {caption}')
+    PrintAlert('Done with adjustment {.val {caption}}')
   }
 
   return(results)
