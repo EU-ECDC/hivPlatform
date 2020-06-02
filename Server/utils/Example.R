61422848a3195694c0f16f3dd1a9c4a2609b85cd
Daniel Lewandowski <daniel@nextpagesoft.net>
2020-06-02 08:15:10 +0200

    Server: clean up example

--- Server/utils/Example.R
+++ Server/utils/Example.R
@@ -1,35 +1,40 @@
 library(hivModelling)
 library(hivEstimatesAccuracy2)
 
+# Define count of MI data sets
 M <- 2
+
+# Define count of bootstrap data sets
 B <- 10
+
+# STEP 1 - Load case-based dataset -----------------------------------------------------------------
 
 appMgr <- AppManager$new()
 appMgr$ReadCaseBasedData('D:/VirtualBox_Shared/dummy_miss1.zip')
 appMgr$PreProcessCaseBasedData()
 appMgr$ApplyOriginGrouping('REPCOUNTRY + UNK + OTHER')
 
-# STEP 1 - Perform MI as usual to obtain M pseudo-complete datasets --------------------------------
+# STEP 2 - Perform MI as usual to obtain M pseudo-complete datasets --------------------------------
 
 adjustmentSpecs <- GetAdjustmentSpecs(c(
   'Multiple Imputation using Chained Equations - MICE'
 ))
 appMgr$AdjustCaseBasedData(adjustmentSpecs, miCount = M)
 
-# STEP 2 - Fit the model to M pseudo-complete datasets get the estimates ---------------------------
+# STEP 3 - Fit the model to M pseudo-complete datasets get the estimates ---------------------------
 
 appMgr$FitHIVModelToAdjustedData(
   settings = list(Verbose = FALSE)
 )
 
-# STEP 3 - Generate B bootstrapped case-based datasets for each pseudo-complete datasets -----------
+# STEP 4 - Generate B bootstrapped case-based datasets for each pseudo-complete datasets -----------
 
 appMgr$GenerateBoostrapCaseBasedDataSets(bsCount = B)
 
-# STEP 4 - Fit the model to M x B bootstrapped case-based datasets ---------------------------------
+# STEP 5 - Fit the model to M x B bootstrapped case-based datasets ---------------------------------
 
 appMgr$FitHIVModelToBootstrapData()
 
-# STEP 5 - Calculate statistics for every output column --------------------------------------------
+# STEP 6 - Calculate statistics for every output column --------------------------------------------
 
 appMgr$ComputeHIVBootstrapStatistics()
