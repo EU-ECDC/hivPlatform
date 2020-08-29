90271743faf7100254c58e3ce972bd952bf4c57e
Daniel Lewandowski <daniel@nextpagesoft.net>
2020-08-29 13:05:23 +0200

    Update dependencies

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
-import 'mobx-react-lite/batchingForReactDom';
 import $ from 'jquery';
 import App from './App';
 import AppManager from './stores/AppManager';
@@ -20,13 +19,13 @@ ReactDOM.render(
   document.getElementById('app'),
   () => {
     console.log('ReactDOM.render complete');
-    $(document).ready(() => {
+    $(() => {
       $(document).on('shiny:connected', () => appManager.setShinyState('CONNECTED'));
       $(document).on('shiny:sessioninitialized', () => appManager.setShinyState('SESSION_INITIALIZED'));
       $(document).on('shiny:disconnected', () => appManager.setShinyState('DISCONNECTED'));
-      $(document).on('shiny:message', () => appManager.setShinyMessage(event.data));
-      $(document).on('shiny:inputchanged', () => console.log('shiny:inputchanged:', event.data));
-      $(document).on('shiny:filedownload', () => console.log('shiny:filedownload:', event.data));
+      $(document).on('shiny:message', event => appManager.setShinyMessage(event));
+      $(document).on('shiny:inputchanged', event => console.log('shiny:inputchanged:', event));
+      $(document).on('shiny:filedownload', event => console.log('shiny:filedownload:', event));
     });
     runInit(appManager);
   },
