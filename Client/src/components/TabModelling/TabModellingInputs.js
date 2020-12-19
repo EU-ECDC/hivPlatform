9c2addb44f9cca899564569532d4aca21505be3f
Daniel Lewandowski <daniel@nextpagesoft.net>
2020-12-19 20:20:30 +0100

    Migrate yarn to version 2

--- Client/src/components/TabModelling/TabModellingInputs.js
+++ Client/src/components/TabModelling/TabModellingInputs.js
@@ -6,8 +6,6 @@ import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
