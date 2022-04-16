38f8af263b6aa9980fb9a7f2b1be96732fb675c2
Daniel Lewandowski <daniel@nextpagesoft.net>
2022-04-16 11:58:52 +0200

    Fix compilation note on constexpr

--- Server/inst/include/const.h
+++ Server/inst/include/const.h
@@ -1,7 +1,7 @@
 #ifndef _hivPlatform_consts_
 #define _hivPlatform_consts_
 
-static constexpr double halfLog2Pi = -0.5 * std::log(2.0 * M_PI);
+static const double halfLog2Pi = -0.5 * std::log(2.0 * M_PI);
 static const Rcpp::NumericVector knotsAge = Rcpp::NumericVector::create(25, 35, 45);
 static const Rcpp::NumericVector knotsCalendar = Rcpp::NumericVector::create(16, 22);
 
