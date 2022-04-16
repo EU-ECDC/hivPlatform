3e900a8bad1a8ec77e3ea2b4cdbcae0a1e4079a7
Daniel Lewandowski <daniel@nextpagesoft.net>
2022-04-16 12:36:02 +0200

    Redefine NDEBUG after RcppArmadillo undefining it

--- Server/inst/include/header.h
+++ Server/inst/include/header.h
@@ -5,13 +5,15 @@
 // [[Rcpp::depends(RcppEigen)]]
 // [[Rcpp::depends(RcppNumerical)]]
 
-#define NDEBUG true
-#define ARMA_NO_DEBUG true
-#define BOOST_DISABLE_ASSERTS true
-#define EIGEN_NO_DEBUG true
-#define EIGEN_NO_STATIC_ASSERT true
+#define NDEBUG
+#define ARMA_NO_DEBUG
+#define BOOST_DISABLE_ASSERTS
+#define EIGEN_NO_DEBUG
+#define EIGEN_NO_STATIC_ASSERT
 
 #include <RcppArmadillo.h>
+// RcppArmadillo undefines NDEBUG. Define it again.
+#define NDEBUG
 #include <Rcpp.h>
 #include <RcppEigen.h>
 #include <RcppNumerical.h>
