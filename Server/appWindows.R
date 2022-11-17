91144aa0efa0b146b9830e6d8054fa5c35a82787
Daniel Lewandowski <daniel@nextpagesoft.net>
2022-11-17 21:48:45 +0100

    Use installed hivPlatform package in appWindows.R

--- Server/appWindows.R
+++ Server/appWindows.R
@@ -1,20 +1,3 @@
-tryCatch({
-  pb <- winProgressBar(
-    title = sprintf('HIV Platform'),
-    label = 'Starting application in default web browser',
-    initial = 1
-  )
-
-  Sys.setenv(RSTUDIO_PANDOC = file.path(dirname(getwd()), 'dist/pandoc'))
-
-  # Load hivPlatform package
-  pkgload::load_all(path = '.', export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
-
-  close(pb)
-
-  options(shiny.maxRequestSize = 100 * 1024^2)
-  hivPlatform::RunApp(launchBrowser = TRUE, port = NULL)
-},
-finally = {
-  close(pb)
-})
+Sys.setenv(RSTUDIO_PANDOC = file.path(dirname(getwd()), 'dist/pandoc'))
+options(shiny.maxRequestSize = 100 * 1024^2)
+hivPlatform::RunApp(launchBrowser = TRUE, port = NULL)
