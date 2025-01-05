f03c277e542d9c4b4d01f2fbe9449eb8d0de51c6
Daniel Lewandowski <daniel@nextpagesoft.net>
2025-01-05 21:49:29 +0100

    Add install script

---
+++ Server/utils/Install.R
@@ -0,0 +1,25 @@
+# Create folder for R packages library and set it as the default library
+rootPath <- "D:/_DEPLOYMENT/hivPlatform"
+
+# Define the GitHub repository reference
+githubRepoRef <- "nextpagesoft/hivPlatform/Server@updates"
+
+# Define the path to the R packages library
+libPath <- file.path(rootPath, "library")
+
+# Create the folder for the R packages library (if it does not exist).
+# REQUIRED TO RUN ONLY ONCE FOR A GIVEN `libPath`.
+dir.create(libPath, recursive = TRUE, showWarnings = FALSE)
+
+# Set the R packages library as the default library
+.libPaths(libPath)
+
+# Install the pak package.
+# REQUIRED TO RUN ONLY ONCE FOR A GIVEN `libPath`.
+install.packages("pak")
+
+# Install the hivPlatform package
+pak::pkg_install(githubRepoRef, dependencies = "hard", upgrade = TRUE, ask = FALSE)
+
+# Run the hivPlatform package
+hivPlatform::RunApp(launchBrowser = TRUE, stopOnSessionEnded = TRUE)
